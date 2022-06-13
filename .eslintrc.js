cd2cfc8548b4d7d0e6a4172fbf76d7cbaddeb987
Pavlo K <80488614+pavlo-kartavkin@users.noreply.github.com>
2022-06-13 09:02:13 +0200

    Added eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'no-plusplus': ['error', {'allowForLoopAfterthoughts': true}]
+  },
+  'globals': {
+    'cy': true,
+    'Cypress': true,
+    'assert': true,
+    'expect': true
+  }
+};
