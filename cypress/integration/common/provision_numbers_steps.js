cccebea6a7889fa3124b56d76edf9dbbf5217603
Pavlo K <80488614+pavlo-kartavkin@users.noreply.github.com>
2022-09-26 16:02:57 +0200

    CYP-325: Unsolicited message settings - bot operated (#101)
    * CYP-325: Unsolicited message settings - bot response
    * CYP-342: Unsolicited message settings - agent response (#102)

---
+++ cypress/integration/common/provision_numbers_steps.js
@@ -0,0 +1,65 @@
+import { And } from 'cypress-cucumber-preprocessor/steps';
+import {convertDataTableIntoDict, validateInputParamsAccordingToDict} from '../../support/utils';
+
+And('Create provision number', (datatable) => {
+  const provisionNumberData = convertDataTableIntoDict(datatable);
+  const requiredParametersAndAcceptableValues = {
+    name: 'any',
+    areaCode: 'any',
+    office: 'any',
+    responseType: ['bot', 'agent', 'none'],
+    saveProvisionNumberAs: 'any',
+  };
+  validateInputParamsAccordingToDict(provisionNumberData, requiredParametersAndAcceptableValues);
+
+  const createProvisionNumberButton = 'button[aria-label="Provision Reusable Number"]';
+  const provisionNumberButton = 'button.MuiButton-outlinedPrimary';
+  const areaCode = '#provisionNumberAreaCode';
+  const numbers = '[aria-label="Available phone numbers"]>.MuiListItem-root';
+  const firstAvailableNumber = '[aria-label="Available phone numbers"]>div:nth-child(1)';
+  const confirmNumberButton = '.MuiDialogActions-root>button:nth-child(2)';
+  const officeDropdown = '#campusOffice';
+
+  cy.get(createProvisionNumberButton).click();
+  cy.get(provisionNumberButton).click();
+  cy.get(areaCode).type(provisionNumberData.areaCode);
+  cy.get(numbers).should('have.length.gt', 3);
+
+  cy.get(firstAvailableNumber).click();
+  cy.get(firstAvailableNumber).invoke('text')
+    .then((provisionNumber) => {
+      cy.wrap(provisionNumber).as(provisionNumberData.saveProvisionNumberAs);
+    });
+  cy.get(confirmNumberButton).click();
+
+  cy.replacePlaceholder(provisionNumberData.name).then((provisionNumberName) => {
+    cy.get('#name').type(provisionNumberName);
+  });
+
+  cy.get('#description').type(provisionNumberData.description);
+  cy.get(officeDropdown).prev().click();
+  cy.contains('span.MuiTypography-displayBlock', provisionNumberData.office).click();
+
+  cy.get('#campaignType').click();
+  cy.contains('[aria-labelledby="campaignType-label"]>li.MuiListItem-root', provisionNumberData.responseType).click();
+
+  cy.get('#inboxSubscriptions').click();
+  cy.contains('span.MuiListItemText-primary', provisionNumberData.inbox).click();
+  cy.contains('button>span', 'Save').click();
+  cy.contains('#notistack-snackbar', 'Saved Successfully!').should('exist');
+});
+
+And('Delete provision number {string}', (provisionNumber) => {
+  const searchInput = 'input[name="keywords"]';
+  const row = 'tbody.MuiTableBody-root>tr';
+  const deleteButton = 'button[aria-label="Revoke number"]';
+  const confirmDeleteButton = '.MuiDialogActions-root>button:nth-child(2)>span';
+
+  cy.replacePlaceholder(provisionNumber).then((number) => {
+    cy.get(searchInput).type(number);
+  });
+  cy.get(row).should('have.length',1);
+  cy.get(deleteButton).click();
+  cy.get(confirmDeleteButton).click();
+  cy.contains('#notistack-snackbar', 'Phone number revoked').should('exist');
+});
