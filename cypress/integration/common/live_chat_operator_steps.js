1ce228dda4467875e15315cb37ee4788449e3ec7
amanocelotbot <103404516+amanocelotbot@users.noreply.github.com>
2022-11-28 13:07:44 +0530

    Update live_chat_operator_steps.js (#191)

--- cypress/integration/common/live_chat_operator_steps.js
+++ cypress/integration/common/live_chat_operator_steps.js
@@ -6,7 +6,7 @@ And('Set operator status to {string}', (status) => {
     throw Error(`Unsupported value ${status}`);
   }
   cy.log(`Setting operator status: ${status}`);
-  const profile = 'header>div>button>span>span>span.MuiBadge-root>div';
+  const profile = 'header>div>button>span>span.MuiBadge-root>div';
   cy.get(profile).click({force: true});
 
   const profileMenu = 'ul[role="menu"]';
@@ -15,7 +15,7 @@ And('Set operator status to {string}', (status) => {
     const isOperatorOnline = body.find('span.Mui-checked').length > 0;
     if ((isOperatorOnline && status === 'away') ||
       (!isOperatorOnline && status === 'available')) {
-      cy.get(chatStatusSwitch).click();
+      cy.get(chatStatusSwitch).click({force: true});
       // ToDo: sometimes page doesn't contain notifications, will uncomment later
       // cy.get('#notistack-snackbar', {timeout: 5000}).should('be.visible');
     }
