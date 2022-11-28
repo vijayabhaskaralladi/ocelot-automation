8691a3c381592a79813c57e94a40886a93327e06
amanocelotbot <103404516+amanocelotbot@users.noreply.github.com>
2022-11-28 18:38:27 +0530

    CYP -438 _ Who_Online_Feature Fixes (#196)
    Added network intercept within reusuable step

--- cypress/integration/common/live_chat_operator_steps.js
+++ cypress/integration/common/live_chat_operator_steps.js
@@ -11,6 +11,13 @@ And('Set operator status to {string}', (status) => {
 
   const profileMenu = 'ul[role="menu"]';
   const chatStatusSwitch = 'span>[type="checkbox"]';
+  cy.replacePlaceholder('${GRAPHQL_URL}graphql').then((url) => {
+    cy.intercept(url, (req) => {
+      if (JSON.stringify(req.body).includes('GetLiveChatOperatorStatus')) {
+        req.alias = 'getOperatorRequest';
+      }
+    });
+  });
   cy.get(profileMenu).then((body) => {
     const isOperatorOnline = body.find('span.Mui-checked').length > 0;
     if ((isOperatorOnline && status === 'away') ||
@@ -20,6 +27,7 @@ And('Set operator status to {string}', (status) => {
       // cy.get('#notistack-snackbar', {timeout: 5000}).should('be.visible');
     }
   });
+  cy.wait('@getOperatorRequest');
   // closing the menu
   cy.get('body').click(0,0);
 });
